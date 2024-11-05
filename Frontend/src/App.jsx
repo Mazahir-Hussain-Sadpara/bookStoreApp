e6cea2ff2756ec5d7cb353e01047d603fb724166
Mazahir-Hussain-Sadpara <mazahirhussain354@gmail.com>
2024-11-05 11:45:56 -0800

    Frontend Don!!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Home from './components/Home/Home'
+import Courses from './components/courses/Courses'
+import Signup from './components/signup'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+    <div className='dark:bg-slate-900 dark:text-white'>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/course' element={<Courses/>} />
+      <Route path='/signup' element={<Signup/>} />
+    </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App
